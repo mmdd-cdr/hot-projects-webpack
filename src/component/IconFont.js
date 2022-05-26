63d466562e9f2e38b3cabaadd63d784fa5e1ffd2
czx <742046286@qq.com>
2022-05-26 16:20:18 +0800

    添加eslint

--- src/component/IconFont.js
+++ src/component/IconFont.js
@@ -1,17 +1,18 @@
-import * as React from "react";
+import * as React from 'react';
 
-const IconFont = ({ id, ...props }) => (
-  <svg
-    className="icon"
-    width="1em"
-    height="1em"
-    fill="currentColor"
-    overflow="hidden"
-    aria-hidden="true"
-    {...props}
-  >
-    <use href={`#${id}`}></use>
-  </svg>
-);
+function IconFont({ id, className }) {
+  return (
+    <svg
+      className={`icon ${className}`}
+      width="1em"
+      height="1em"
+      fill="currentColor"
+      overflow="hidden"
+      aria-hidden="true"
+    >
+      <use href={`#${id}`} />
+    </svg>
+  );
+}
 
 export default IconFont;
