1c21f12f1a564d63a880718afb227930e051366b
czx <742046286@qq.com>
2022-05-27 09:52:21 +0800

    提交代码

--- src/component/header/index.jsx
+++ src/component/header/index.jsx
@@ -10,8 +10,7 @@ const Header = () => {
     <div className="header-button" >
       <Link className={`header-nav ${pageLocation.pathname === "/" ? 'active' : ''}`} to="/" >Popular</Link>
     </div>
-
-            <div className="header-button" >
+    <div className="header-button" >
       <Link className={`header-nav ${pageLocation.pathname === "/battle" ? 'active' : ''}`} to="/battle" >Battle</Link>
     </div>
   </div>
